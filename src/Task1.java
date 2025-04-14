6fc8949327856011c94425c158a0b2c3f919aa69
Hameza <gamezo2021@inbox.ru>
2025-04-14 16:39:48 +0300

    Task1 done

---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+/** Задание 1
+Вывести Hello World. Использовать метод.
+*/
+
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        print();
+    }
+    public static void print() {
+        System.out.print("Hello World");
+    }
+}
