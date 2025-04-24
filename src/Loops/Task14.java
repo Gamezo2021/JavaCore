a8c189dfa396995aa929f478a0040a5f1daee1ad
gamezo2021 <gamezo2021@inbox.ru>
2025-04-24 20:51:23 +0300

    Task 14 done

--- src/Loops/Task14.java
+++ src/Loops/Task14.java
@@ -1,18 +1,20 @@
 package Loops;
 
+
 public class Task14 {
     public static void main(String[] args) {
         long a = 123456789;
-        System.out.print( sumOfNumbers(a));
+        System.out.print(sumOfNumbers(a));
     }
 
     public static long sumOfNumbers(long a) {
         long sum = 0;
         int b = 0;
         do {
+
+            b = (int) (a % 10);
             sum += b;
-           b= (int) (a%10);
-           a = a/10;
+            a = a / 10;
         } while (a > 0);
         return sum;
     }
