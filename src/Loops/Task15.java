17c200daec39c7256407e1b6237a7cbd2cf43779
Hameza <gamezo2021@inbox.ru>
2025-04-25 12:50:20 +0300

    Task 15 done

---
+++ src/Loops/Task15.java
@@ -0,0 +1,29 @@
+package Loops;
+
+/**
+ * Задание 15
+ * <p>
+ * Найти среди чисел из заданного промежутка второе простое число
+ * (число называют простым, если оно делится без остатка только на 1 и себя) и завершить цикл с использованием break.
+ * Логика в методах. System.out.println() в main. Не использовать классы для определения простого числа.
+ */
+
+public class Task15 {
+    public static void main(String[] args) {
+        System.out.print(primeNumber());
+    }
+
+    public static int primeNumber() {
+
+        int i;
+        for (i = 1; i <= 10; ++i) {
+            if (i / i == 1 && i / 1 == i) {
+                if (i == 2 || i == 1) {
+                    continue;
+                }
+                break;
+            }
+        }
+        return i;
+    }
+}
