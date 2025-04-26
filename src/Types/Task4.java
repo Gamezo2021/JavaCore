e1dfd8fd53ed90d9be628dd4fbe14a3ea31ed44b
Hameza <gamezo2021@inbox.ru>
2025-04-26 21:50:53 +0300

    task4 refactoring

--- src/Types/Task4.java
+++ src/Types/Task4.java
@@ -10,7 +10,7 @@ package Types;
 
 public class Task4 {
     public static void main(String[] args) {
-        System.out.print(mathOperation(2,10));
+        System.out.println(mathOperation(2,10));
     }
     public static int mathOperation(int a, int b) {
         return (a + b) + (a * b);
