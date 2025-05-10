7c62e81a565a93ff1a9332b371c3f46421dec368
Hameza <gamezo2021@inbox.ru>
2025-05-10 19:15:28 +0300

    OperatorsFinalTask done

--- src/Operators/OperatorsFinalTask.java
+++ src/Operators/OperatorsFinalTask.java
@@ -11,17 +11,18 @@ package Operators;
 public class OperatorsFinalTask {
     public static void main(String[] args) {
         int x = 1;
-        int y = 2;
+        int y = 3;
         int z = 3;
         System.out.print(mathCheck(x, y, z));
     }
 
     public static String mathCheck(int x, int y, int z) {
         String s = " ";
+        float arithmeticMean = (x + y + z) / 3f;
         if (x > z) {
             s = "Сумма X + y = " + (x + y);
         } else
             s = "z = " + z;
-        return s + " Среднее арифметическое = " + (x + y + z) / 3;
+        return s + " Среднее арифметическое = " + arithmeticMean;
     }
 }
