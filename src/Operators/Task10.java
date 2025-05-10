3518c57230e1effbb9acea8d7ba7242129807c1d
Hameza <gamezo2021@inbox.ru>
2025-05-10 19:03:38 +0300

    Task 10 done

--- src/Operators/Task10.java
+++ src/Operators/Task10.java
@@ -10,20 +10,22 @@ package Operators;
 
 public class Task10 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 40;
-        int c = 10;
-        int d = 30;
-        int e = 50;
-        int f = 60;
+        int a = 12;
+        int b = 4;
+
+        int c = 1;
+        int d = 7;
+
+        int e = 100;
+        int f = 10;
         System.out.print(CapacityСheck(a, b, c, d, e, f));
     }
 
     public static String CapacityСheck(int a, int b, int c, int d, int e, int f) {
-        int sitePerimeter = e * f;
-        int FirstHousePerimeter = a * b;
-        int SecondHousePerimeter = c * d;
-        if (sitePerimeter >= FirstHousePerimeter + SecondHousePerimeter && e >= a + c && f >= b + d && e >= (a | c) && f >= (b | d)) {
+        int siteSquare = e * f;
+        int FirstHouseSquare = a * b;
+        int SecondHouseSquare = c * d;
+        if ((siteSquare >= FirstHouseSquare + SecondHouseSquare && e >= a + c && f >= b + d && e >= (a | c) && f >= (b | d)) || (e >= a && e >= c && f >= b + d) || (e >= a + c && f >= b && f >= d)) {
             return "Yes";
         }
         return "No";
