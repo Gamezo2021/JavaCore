426ac48ed22de8b71169e85bae6b461836124de9
gamezo2021 <gamezo2021@inbox.ru>
2025-08-12 14:33:18 +0300

    Task 24 done

---
+++ src/Objects/Task24/PeriodOfTime.java
@@ -0,0 +1,64 @@
+package Objects.Task24;
+
+public class PeriodOfTime implements Comparable<PeriodOfTime> {
+    private int seconds;
+    private int minutes;
+    private int hour;
+
+    public PeriodOfTime(int seconds, int minutes, int hour) {
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hour = hour;
+    }
+
+    public PeriodOfTime(int seconds) {
+        this.hour = seconds / 3600;
+        this.minutes = seconds % 3600 / 60;
+        this.seconds = seconds % 60;
+
+
+    }
+
+    public int timeInSeconds(int seconds, int minutes, int hour) {
+        int result = (hour * 3600) + (minutes * 60) + seconds;
+        return result;
+    }
+
+    public void printTime() {
+        System.out.println("Секунд " + seconds + " минут " + minutes + " часов " + hour);
+    }
+
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    @Override
+    public int compareTo(PeriodOfTime o) {
+        if (this.timeInSeconds(this.seconds, this.minutes, this.hour) > o.timeInSeconds(o.seconds, o.minutes, o.hour)) {
+            return 1;
+        } else if (this.timeInSeconds(this.seconds, this.minutes, this.hour) < o.timeInSeconds(o.seconds, o.minutes, o.hour)) {
+            return -1;
+        } else return 0;
+    }
+}
---
+++ src/Objects/Task24/Task24.java
@@ -0,0 +1,31 @@
+package Objects.Task24;
+
+/**
+ * Создать класс описывающий промежуток времени.
+ * Сам промежуток в классе должен заваться тремя свойтсвами: секундами, минутами, часами.
+ * Сделать методы для получения полного количества секундр в обьекте, сравнения двух обьектов ( метод должен работать аналогично compereTo в строках).
+ * Создать два констуртора :
+ * 1) получающий общее количество секунд.
+ * 2) получающий часы, минуты и секунды по отдельности.
+ * Сделать метод лоя вывода данных.
+ */
+public class Task24 {
+    public static void main(String[] args) {
+        PeriodOfTime periodOfTime = new PeriodOfTime(109210);
+        PeriodOfTime periodOfTime1 = new PeriodOfTime(10, 20, 30);
+        periodOfTime.printTime();
+        System.out.println("Общее колличество секунд " + periodOfTime.timeInSeconds(periodOfTime.getSeconds(), periodOfTime.getMinutes(), periodOfTime.getHour()));
+        periodOfTime1.printTime();
+        System.out.println("Общее колличество секунд " + periodOfTime1.timeInSeconds(periodOfTime1.getSeconds(), periodOfTime1.getMinutes(), periodOfTime1.getHour()));
+
+        System.out.println(periodOfTime.compareTo(periodOfTime1));
+
+
+    }
+
+
+
+
+
+}
+
